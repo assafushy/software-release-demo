f3f3caf14075867de9251665cac409302410f867
Mohamed Abomokh <abomokhm97@gmail.com>
2023-07-30 17:13:55 +0000

    updated route

--- routes/mohamed-route.js
+++ routes/mohamed-route.js
@@ -1,5 +1,5 @@
 module.exports = (app) => {
-  app.get("mohamed-test", (req, res) => {
+  app.get("/mohamed", (req, res) => {
     res.send("Hello world,This is an excellent route written by the Mo!");
   });
 };
