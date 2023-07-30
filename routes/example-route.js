b9815bd47f55353d78f40bae37fb17a3837ee9d5
YudaDv <yudadvir@gmail.com>
2023-07-30 16:51:24 +0000

    added yuda route

--- routes/example-route.js
+++ routes/example-route.js
@@ -1,5 +1,5 @@
 module.exports = (app) => {
-  app.get("/example", (req, res) => {
-    res.send("This is an example route!");
+  app.get("/yuda", (req, res) => {
+    res.send("This is an DAAAMN!");
   });
 };
